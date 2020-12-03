9e26cf982188ee01d434f9f2cb27c446eb04af46
Sylvie-B <unsolo@hotmail.fr>
2020-12-03 12:59:40 +0100

    essai de la fin de l'exo 1

--- exo 1/script.js
+++ exo 1/script.js
@@ -1,7 +1,12 @@
 let list01 = document.getElementById("liste-commissions");
-console.log(list01);
 
 let newItem = "une boite d'oeufs";
 
 list01.innerHTML += "<li>" + newItem + "</li>";
+
+function retour (){
+    let newNewItem = document.createElement("li");
+    newNewItem.innerHTML = document.getElementById("ajout").value;
+    document.getElementById("liste-commissions").append(newNewItem);
+}
 
