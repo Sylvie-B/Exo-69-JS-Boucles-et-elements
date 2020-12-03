04ac6661f519f2246eb81cd318a1da527e11132d
Sylvie-B <unsolo@hotmail.fr>
2020-12-03 22:25:02 +0100

    re essai exo 1

--- exo 1/script.js
+++ exo 1/script.js
@@ -1,12 +1,14 @@
 let list01 = document.getElementById("liste-commissions");
 
 let newItem = "une boite d'oeufs";
-
 list01.innerHTML += "<li>" + newItem + "</li>";
 
-function retour (){
+
+let button = document.getElementById("bouton");
+
+button.addEventListener("click", function (){
     let newNewItem = document.createElement("li");
-    newNewItem.innerHTML = document.getElementById("ajout").value;
-    document.getElementById("liste-commissions").append(newNewItem);
-}
+    newNewItem.innerHTML  = document.getElementById("addItem").innerHTML;
+    list01.append(newNewItem);
+});
 
